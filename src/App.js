549a5db00d56d47d04b94c521638ed58b480879a
Eemeli Saarinen <saarineneemelia@gmail.com>
2022-12-22 07:51:21 +0200

    Initial commit, simple country quiz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+import axios from 'axios'
+
+var data = require('./data.json');
+
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [score, setScore] = useState(0)
+
+  const newCountries = () => {
+    var len = data.length;
+    var idx1 = Math.floor(Math.random() * len);
+    var idx2 = Math.floor(Math.random() * len);
+    while(idx1 === idx2) {
+      idx2 = Math.floor(Math.random() * len);
+    }
+    var country1 = data[idx1];
+    var country2 = data[idx2];
+    return [country1, country2];
+  }
+
+  useEffect(() => {
+    console.log('effect')
+    var c = newCountries();
+    setCountries(c)
+  }, [])
+  
+  console.log(countries[0].population)
+  const clickLeft = () => {
+    if (countries[0].population > countries[1].population) {
+      var c = newCountries();
+      setCountries(c);
+      var s = score + 1;
+      setScore(s);
+    } else {
+      setScore(0);
+    }
+  }
+
+  const clickRight = () => {
+    if (countries[0].population < countries[1].population) {
+      var c = newCountries();
+      setCountries(c);
+      var s = score + 1;
+      setScore(s);
+    } else {
+      setScore(0);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", width: "100%", backgroundColor:"#252525" }}>
+      <div style={{ display: "table", margin:"0 auto" }}><p style={{fontSize: "30px", color:"red"}}>Score: {score}</p></div>
+      <div style={{ height: "80vh", width: "48%", display:"flex", justifyContent:"center", alignItems:"center", float:"left", backgroundImage:`url(${countries[0].flags.png})`, backgroundRepeat:"no-repeat", backgroundSize: "100%", backgroundPositionY: "center",}} >
+        <button style={{ fontSize:"50px", backgroundColor:"white", borderStyle:"solid", borderColor:"black", borderWidth:"5px", padding:"20px" }}
+          onClick={clickLeft}>
+            { countries[0].name.common }
+        </button>
+      </div>
+      <div style={{ height: "80vh", width: "48%", display:"flex", justifyContent:"center", alignItems:"center", float:"right", backgroundImage:`url(${countries[1].flags.png})`, backgroundRepeat:"no-repeat", backgroundSize: "100%", backgroundPositionY: "center",}} >
+        <button style={{ fontSize:"50px", backgroundColor:"white", borderStyle:"solid", borderColor:"black", borderWidth:"5px", padding:"20px" }}
+          onClick={clickRight}>
+            { countries[1].name.common }
+        </button> 
+      </div>
     </div>
   );
 }
